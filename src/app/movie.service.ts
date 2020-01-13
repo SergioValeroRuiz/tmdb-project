0ccafb3e3d3cba62e1ba2d3993e839feb3a00ae7
sergiovaleroruiz <sergio.vale.ru@gmail.com>
2020-01-13 16:21:16 +0100

    Retrieving data from service

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  tmdbURL = 'https://api.themoviedb.org/3/discover/movie';
+  tmdbKEY = '15d2ea6d0dc1d476efbca3eba2b9bbfb';
+
+  getMovies() {
+    return this.httpClient.get( this.tmdbURL + '?api_key=' + this.tmdbKEY);
+  }
+
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import{ MovieService } from '../movie.service';
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  private movies = [];
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.getMovies();
+  }
+
+  getMovies(): void{
+    this.movieService.getMovies().subscribe((res : any[])=>{
+      console.log(res);
+      this.movies = res['results'];
+  }); 
   }
 
 }
