245df0dddb95708943b7dd27f367cf4ac0eae765
sergiovaleroruiz <sergio.vale.ru@gmail.com>
2020-01-13 16:00:29 +0100

    Components and services are generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviePopupComponent } from './movie-popup/movie-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MoviePopupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-popup/movie-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-popup',
+  templateUrl: './movie-popup.component.html',
+  styleUrls: ['./movie-popup.component.sass']
+})
+export class MoviePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
